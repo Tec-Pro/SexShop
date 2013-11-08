25d15ffb1aa538fe34c551ba01e2b8b4608f6743
jheredia <joaquin.heredia93@gmail.com>
2013-11-08 15:33:26 -0300

    Empezamos

---
+++ src/abm/ABMInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abm;
+
+/**
+ *
+ * @author joako
+ */
+interface ABMInterface {
+    
+    void alta(Object o);
+    
+    void baja(Object o);
+    
+    void modificar(Object o);
+}
