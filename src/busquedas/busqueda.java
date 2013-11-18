c3f249433eb03efa816b722bf3f6bbb35f858b69
jheredia <joaquin.heredia93@gmail.com>
2013-11-18 18:20:11 -0300

    Busquedas agregadas

--- src/busquedas/busqueda.java
+++ src/busquedas/busqueda.java
@@ -39,15 +39,33 @@ public class busqueda {
         return result;
     }
     
-   /* public List<Compra> filtroCompra(String idproveedor, String fecha){
+   public List<Compra> filtroCompra(String idproveedor, String fecha){
         List<Compra> result;
         result = Compra.where("idproveedor like ? and fecha like ?", "%"+idproveedor+"%","%"+fecha+"%");
         return result;
-    }*/
+    }
     
-    public List<Proveedor> filtroProveedor(String cuil, String apellido) {
+    public List<Proveedor> filtroProveedor(String cuil,String nombre, String apellido) {
         List<Proveedor> result;
-        result = Proveedor.where("cuil like ? and apellido like ?", "%"+cuil+"%","%"+apellido+"%");
+        result = Proveedor.where("cuil like ? and nombre like ? and apellido like ?", "%"+cuil+"%","%"+nombre+"%","%"+apellido+"%");
+        return result;
+    }
+    
+    public List<ProductosComprado> filtroComprados(String idcompra, String idproducto){
+        List<ProductosComprado> result;
+        result = ProductosComprado.where("idcompra like ? and idproducto like ?", "%"+idcompra+"%","%"+idproducto+"%");
+        return result;
+    }
+     
+    public List<ProductosVendido> filtroVendidos(String idventa, String idproducto){
+        List<ProductosVendido> result;
+        result = ProductosVendido.where("idventa like ? and idproducto like ?", "%"+idventa+"%","%"+idproducto+"%");
+        return result;
+    }
+    
+     public List<Adquirido> filtroAdquiridos(String idcliente, String idproducto){
+        List<Adquirido> result;
+        result = Adquirido.where("idcliente like ? and idproducto like ?", "%"+idcliente+"%","%"+idproducto+"%");
         return result;
     }
 }
