a8dd59338fb8c48534e71e027d74aff823e67b6d
lputruele <lputruele@gmail.com>
2013-11-18 17:30:59 -0300

    abmproveedor

--- src/abm/ABMProveedor.java
+++ src/abm/ABMProveedor.java
@@ -18,29 +18,34 @@ public class ABMProveedor implements ABMInterface<Proveedor> {
     }
 
     @Override
-    public void alta(Proveedor p) {
+    public boolean alta(Proveedor p) {
        if (!findProveedor(p)){
             Proveedor nuevo = Proveedor.create("nombre",p.get("nombre"),"apellido",p.get("apellido"),"telefono",p.get("telefono"),"celular",p.get("celular"),"mail",p.get("mail"),"cuil",p.get("cuil"),"nombre_banco",p.get("nombre_banco"),"sucursal",p.get("sucursal"),"tipo_cuenta",p.get("tipo_cuenta"),"cuenta",p.get("cuenta"),"compra_minima",p.get("compra_minima"));
-            nuevo.saveIt();  
+            nuevo.saveIt();
+            return true;
         } else{
             System.out.println("Proveedor ya existente");
+            return false;
         } 
     }
 
     @Override
-    public void baja(Proveedor p) {
-        Proveedor viejo = Proveedor.findFirst("cuil = ?", p.get("cuil"));
-        if (findProveedor(viejo)){
-            viejo.delete();
+    public boolean baja(Proveedor p) {
+        if (findProveedor(p)){
+            p.delete();
+            return true;
         }
+        else return false;
     }
 
     @Override
-    public void modificar(Proveedor p) {
-        Proveedor viejo = Proveedor.findFirst("cuil = ?", p.get("cuil"));
-       if (findProveedor(viejo)){
+    public boolean modificar(Proveedor p) {
+       Proveedor viejo = Proveedor.findFirst("cuil = ?", p.get("cuil"));
+       if (viejo!=null){
             viejo.set("nombre",p.get("nombre"),"apellido",p.get("apellido"),"telefono",p.get("telefono"),"celular",p.get("celular"),"mail",p.get("mail"),"cuil",p.get("cuil"),"nombre_banco",p.get("nombre_banco"),"sucursal",p.get("sucursal"),"tipo_cuenta",p.get("tipo_cuenta"),"cuenta",p.get("cuenta"),"compra_minima",p.get("compra_minima")).saveIt();
+            return true;
        }
+       else return false;
     }
     
 }
