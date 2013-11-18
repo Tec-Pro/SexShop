9ca5e34f5c06218862e2be34513a6e052014ac7a
lputruele <lputruele@gmail.com>
2013-11-17 23:13:02 -0300

    proveedor y abmproveedor activejdbc

--- src/abm/ABMProveedor.java
+++ src/abm/ABMProveedor.java
@@ -4,76 +4,43 @@
  */
 package abm;
 
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import modelos.Proveedor;
-import java.util.LinkedList;
 import modelos.Producto;
 /**
  *
  * @author joako
  */
 public class ABMProveedor implements ABMInterface<Proveedor> {
+    
+    public boolean findProveedor(Proveedor p){
+        return (Proveedor.first("cuil = ?", p.get("cuil"))!=null);
+    }
 
     @Override
     public void alta(Proveedor p) {
-        if (p.getId()==null)
-        try {
-            Statement st = ConnectionDataBase.getConnection().createStatement();
-            st.executeUpdate("INSERT INTO proveedor(nombre,apellido,dni,telefono,celular,mail,cuil,nombre_banco,sucursal,tipo_cuenta,cuenta,compra_minima) VALUES('"+p.getNombre()+"','"+p.getApellido()+"','"+p.getDni()+"','"+p.getTelefono()+"','"+p.getCelular()+"','"+p.getMail()+"','"+p.getCuil()+"','"+p.getNombreBanco()+"','"+p.getSucursal()+"','"+p.getTipoCuenta()+"','"+p.getCuenta()+"','"+p.getCompraMinima()+"')");
-            LinkedList<Producto> l=p.getProvee();
-            Producto aux;
-            while (!l.isEmpty()){
-               aux=l.removeFirst();
-               st.executeUpdate("INSERT INTO provee VALUES('"+p.getId()+"','"+aux.getNumeroProducto()+"')");
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ABMProveedor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        else return; 
+       if (!findProveedor(p)){
+            Proveedor nuevo = Proveedor.create("nombre",p.get("nombre"),"apellido",p.get("apellido"),"telefono",p.get("telefono"),"celular",p.get("celular"),"mail",p.get("mail"),"cuil",p.get("cuil"),"nombre_banco",p.get("nombre_banco"),"sucursal",p.get("sucursal"),"tipo_cuenta",p.get("tipo_cuenta"),"cuenta",p.get("cuenta"),"compra_minima",p.get("compra_minima"));
+            nuevo.saveIt();  
+        } else{
+            System.out.println("Proveedor ya existente");
+        } 
     }
 
     @Override
     public void baja(Proveedor p) {
-        try {
-            Statement st = ConnectionDataBase.getConnection().createStatement();
-            st.executeUpdate("DELETE FROM proveedor WHERE id = '"+p.getId()+"'");
-        } catch (SQLException ex) {
-            Logger.getLogger(ABMProveedor.class.getName()).log(Level.SEVERE, null, ex);
+        Proveedor viejo = Proveedor.findFirst("cuil = ?", p.get("cuil"));
+        if (findProveedor(viejo)){
+            viejo.delete();
         }
     }
 
     @Override
     public void modificar(Proveedor p) {
-         try {
-            Statement st = ConnectionDataBase.getConnection().createStatement();
-            if (p.getNombre() != null)
-                st.executeUpdate("UPDATE proveedor SET nombre ='"+p.getNombre()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getApellido() != null)
-                st.executeUpdate("UPDATE proveedor SET apellido ='"+p.getApellido()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getTelefono() != null)
-                st.executeUpdate("UPDATE proveedor SET telefono ='"+p.getTelefono()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getCelular() != null)
-                st.executeUpdate("UPDATE proveedor SET celular ='"+p.getCelular()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getMail() != null)
-                st.executeUpdate("UPDATE proveedor SET mail ='"+p.getMail()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getCuil() != null)
-                st.executeUpdate("UPDATE proveedor SET cuil ='"+p.getCuil()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getNombreBanco() != null)
-                st.executeUpdate("UPDATE proveedor SET nombre_banco ='"+p.getNombreBanco()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getSucursal() != null)
-                st.executeUpdate("UPDATE proveedor SET sucursal ='"+p.getSucursal()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getTipoCuenta() != null)
-                st.executeUpdate("UPDATE proveedor SET tipo_cuenta ='"+p.getTipoCuenta()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getCuenta() != null)
-                st.executeUpdate("UPDATE proveedor SET cuenta ='"+p.getCuenta()+"' WHERE id ='"+p.getId()+"'");
-            if (p.getCompraMinima() != null)
-                st.executeUpdate("UPDATE proveedor SET compra_minima ='"+p.getCompraMinima()+"' WHERE id ='"+p.getId()+"'");
-        } catch (SQLException ex) {
-            Logger.getLogger(ABMCliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Proveedor viejo = Proveedor.findFirst("cuil = ?", p.get("cuil"));
+       if (findProveedor(viejo)){
+            viejo.set("nombre",p.get("nombre"),"apellido",p.get("apellido"),"telefono",p.get("telefono"),"celular",p.get("celular"),"mail",p.get("mail"),"cuil",p.get("cuil"),"nombre_banco",p.get("nombre_banco"),"sucursal",p.get("sucursal"),"tipo_cuenta",p.get("tipo_cuenta"),"cuenta",p.get("cuenta"),"compra_minima",p.get("compra_minima")).saveIt();
+       }
     }
     
 }
