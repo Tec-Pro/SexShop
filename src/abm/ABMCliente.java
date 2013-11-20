39703a808c1f9f301da819a1fc4388ddfbe1435b
jheredia <joaquin.heredia93@gmail.com>
2013-11-20 01:07:29 -0300

    Listos ABMCliente y Producto

--- src/abm/ABMCliente.java
+++ src/abm/ABMCliente.java
@@ -5,6 +5,7 @@
 package abm;
 
 import modelos.Cliente;
+import org.javalite.activejdbc.Base;
 
 /**
  *
@@ -14,7 +15,10 @@ public class ABMCliente implements ABMInterface<Cliente>{
 
     
     public Cliente getCliente(Cliente c){
-        return Cliente.first("nombre = ? and apellido = ? and telefono = ?", c.get("nombre"), c.get("apellido"), c.get("telefono"));
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root");
+        Cliente r = Cliente.first("nombre = ? and apellido = ? and telefono = ?", c.get("nombre"), c.get("apellido"), c.get("telefono"));
+        Base.close();
+        return r;
     }
     
     public boolean findCliente(Cliente c){
@@ -24,11 +28,16 @@ public class ABMCliente implements ABMInterface<Cliente>{
     
     @Override
     public boolean alta(Cliente c) {
+           Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root");
         if (!findCliente(c)){
+            Base.openTransaction();
             Cliente nuevo = Cliente.create("nombre",c.get("nombre"),"apellido",c.get("apellido"),"telefono",c.get("telefono"),"celular",c.get("celular"),"mail",c.get("mail"));
-            nuevo.saveIt();  
+            nuevo.saveIt();
+            Base.commitTransaction();
+            Base.close();
             return true;
         } else{
+            Base.close();
             return false;
         }
     }   
@@ -36,21 +45,31 @@ public class ABMCliente implements ABMInterface<Cliente>{
 
     @Override
     public boolean baja(Cliente c) {
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root");
         Cliente viejo = Cliente.findById(c.getId());
         if (viejo!=null){
+            Base.openTransaction();
             viejo.delete();
+            Base.commitTransaction();
+            Base.close();
             return true;
         }
+        Base.close();
         return false;
     }
 
     @Override
     public boolean modificar(Cliente c) {
+       Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root"); 
        Cliente viejo = Cliente.findById(c.getId());
        if (viejo!=null){
+            Base.openTransaction();
             viejo.set("nombre",c.get("nombre"),"apellido",c.get("apellido"),"telefono",c.get("telefono"),"celular",c.get("celular"),"mail",c.get("mail")).saveIt();
+            Base.commitTransaction();
+            Base.close();
             return true;
        }
+       Base.close();
        return false;
     }
 }
--- src/abm/ABMProducto.java
+++ src/abm/ABMProducto.java
@@ -9,6 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import modelos.Producto;
 import modelos.Proveedor;
+import org.javalite.activejdbc.Base;
 
 /**
  *
@@ -23,18 +24,24 @@ public class ABMProducto implements ABMInterface<Producto> {
     
     @Override
     public boolean alta(Producto p) {
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root");
         if (!findProducto(p)){
             Proveedor pr = Proveedor.first("cuil = ?", p.getCuilProveedor());
             if (pr!=null){
+                Base.openTransaction();
                 Producto nuevo = Producto.create("precio_venta",p.get("precio_venta"),"precio_compra",p.get("precio_compra"),"stock",p.get("stock"),"numero_producto",p.get("numero_producto"),"nombre",p.get("nombre"),"tipo",p.get("tipo"),"marca",p.get("marca"));
                 nuevo.saveIt();
                 pr.add(nuevo);
+                Base.commitTransaction();
+                Base.close();
                 return true;
             } else {
+                Base.close();
                 System.out.println("Error proveedor");
                 return false;
             }
         } else {
+            Base.close();
             System.out.println("Error producto");
             return false;
         }
@@ -43,20 +50,30 @@ public class ABMProducto implements ABMInterface<Producto> {
 
     @Override
     public boolean baja(Producto p) {
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root");
         if (findProducto(p)){
+            Base.openTransaction();
             p.delete();
+            Base.commitTransaction();
+            Base.close();
             return true;
         }
+        Base.close();
         return false;
     }
 
     @Override
     public boolean modificar(Producto p) {
+       Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/sexshop", "root", "root");
        Producto viejo = Producto.findFirst("numero_producto = ?", p.get("numero_producto"));
        if (viejo!=null){
+            Base.openTransaction();
             viejo.set("precio_venta",p.get("precio_venta"),"precio_compra",p.get("precio_compra"),"stock",p.get("stock"),"numero_producto",p.get("numero_producto"),"nombre",p.get("nombre"),"tipo",p.get("tipo"),"marca",p.get("marca")).saveIt();
+            Base.commitTransaction();
+            Base.close();
             return true;
        }
+       Base.close();
        return false;
     }
     
