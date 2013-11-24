ff5c47dea7e2968923419fbfe7671676db769b6b
ezensich <ezensich@gmail.com>
2013-11-24 20:05:56 -0300

    Correccion ABM devolucion de stock

--- src/abm/ABMVenta.java
+++ src/abm/ABMVenta.java
@@ -202,13 +202,11 @@ public class ABMVenta implements ABMInterface<Venta> {
             par = null;
             par = (Pair) itr.next(); //saco el par de la lista
             prodViejo = (Producto) par.first(); //saco el producto del par
-            if (prodViejo.exists()){ //si existe el producto en la base
-                cant = (Integer)((Pair) par.second()).first();//saco la cantidad del par
-                cant = prodViejo.getInteger("stock") + cant;//devuelvo el stock anterior a la venta del producto
-                resultOp = resultOp && prodViejo.setInteger("stock", cant).saveIt();//actualizo el stock del producto
-                if (Proveedor.findById(prodViejo.get("proveedor_id"))!=null)
-                    Proveedor.findById(prodViejo.get("proveedor_id")).add(prodViejo);//creo la relacion
-                }
+            cant = (Integer)((Pair) par.second()).first();//saco la cantidad del par
+            cant = prodViejo.getInteger("stock") + cant;//devuelvo el stock anterior a la venta del producto
+            resultOp = resultOp && prodViejo.setInteger("stock", cant).saveIt();//actualizo el stock del producto
+            if (Proveedor.findById(prodViejo.get("proveedor_id"))!=null)
+                Proveedor.findById(prodViejo.get("proveedor_id")).add(prodViejo);//creo la relacion
             }
         return resultOp;
     }
